46ec00dd26cc297fba62595756b537d4641bdb0b
Govindaraj Rangan <govindaraj.rangan@wipro.com>
2018-04-25 15:18:28 -0400

    Added mode to shutdown

--- shutdown.sh
+++ shutdown.sh
@@ -1,1 +1,4 @@
-az group deployment create --resource-group myFarm --template-uri https://raw.githubusercontent.com/GovindarajRangan/farm/master/arm/resourcegroupCleanup.json
+az group deployment create \
+    --resource-group myFarm \
+    --template-uri https://raw.githubusercontent.com/GovindarajRangan/farm/master/arm/resourcegroupCleanup.json \
+    --mode complete
