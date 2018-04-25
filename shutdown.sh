daf3d78ee8e4a43ec0fca6f8df4ecf51023ac0c9
Govindaraj Rangan <govindaraj.rangan@wipro.com>
2018-04-25 14:14:22 -0400

    Added Startup and Shutdown scripts

---
+++ shutdown.sh
@@ -0,0 +1,1 @@
+az group deployment create --resource-group myFarm --template-uri https://raw.githubusercontent.com/GovindarajRangan/farm/master/arm/resourcegroupCleanup.json
---
+++ startup.sh
@@ -0,0 +1,12 @@
+az acs create \
+    --name myFarm \
+    --resource-group myFarm \
+    --master-count 1 \
+    --agent-count 3 \
+    --admin-username govindr \
+    --agent-vm-size Standard_D2_v2 \
+    --dns-prefix myFarm \
+    --ssh-key-value "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEA5rLPzTKtynuUMYFQ9Avwpbmms0d2CZzRh/8a2rrbSRag39zt8rAkXcuzfO3apLkrDKj9PYbO5LJPWXtZpay+STKWvYEdrhhpaT0/7v35BmIoJGj6xchCnHM8J0/C0Osz8FfAbOVjGA9podjYeNnl1KwoIpbb1HOAaYnRRpAFE3zIGzz5LS5tnHrIe2f52YWuIChRkITVNBi6uKSzJ/Q4Nmu8xBmQHAgSa6i2/MEZ3CcCffuaVw48aHCqGXlL/ivBlapvgMKhfam+YynoW88JgOb6eUiuYIbez2kcL1O/MMsiE1BcraX0/+9q4J1X56K4S0eJfwbvULL4cL2JIXVbIw== govindaraj.rangan@wipro.com" \
+    --orchestrator-type kubernetes \
+    --service-principal 3d957a5d-76d3-4b20-acc0-02881c366401 \
+    --client-secret tLIB7wi+mGV8nnQskftuIYeJMr9FSIbBoHxFQwz4ERw=
