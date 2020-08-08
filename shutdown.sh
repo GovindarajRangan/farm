af26e32a8edda7c8f4fcfd1d1d798600dfa35740
GovindarajRangan <govindaraj.rangan@gmail.com>
2020-08-08 10:39:16 -0400

    change in az deployment group syntax

--- shutdown.sh
+++ shutdown.sh
@@ -4,3 +4,8 @@ az group deployment create \
     --resource-group myFarm \
     --template-uri https://raw.githubusercontent.com/GovindarajRangan/farm/master/infra/resourcegroupCleanup.json \
     --mode complete
+
+az deployment group create \
+    --resource-group myFarm \
+    --template-uri https://raw.githubusercontent.com/GovindarajRangan/farm/master/infra/resourcegroupCleanup.json \
+    --mode complete
